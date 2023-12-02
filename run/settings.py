30811b6d2486a3e0fae96e4027f3a2aa7e6f3ac8
Dolldexxa <dressadias00@gmail.com>
2023-12-01 22:27:21 -0300

    A3
    Part 2

--- run/settings.py
+++ run/settings.py
@@ -16,6 +16,8 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPS_DIR = os.path.join(BASE_DIR, 'apps') 
+sys.path.insert(0, APPS_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
