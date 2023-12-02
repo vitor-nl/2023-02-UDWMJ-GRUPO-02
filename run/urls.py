4db47d2925d4b1c0cf30790e543369d7b968d595
Dolldexxa <dressadias00@gmail.com>
2023-12-01 22:36:37 -0300

    A3
    Part 3

--- run/urls.py
+++ run/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('produtos/', include('products.urls', namespace='products')),
     path('categorias/', include('categories.urls', namespace='categories')),
 ]
